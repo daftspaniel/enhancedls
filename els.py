9db68d166dd69e7c06b69542381d2a43755dee81
Davy Mitchell <daftspaniel@gmail.com>
2021-08-29 07:55:37 +0100

    Formatting, comments and tidies.

--- els.py
+++ els.py
@@ -10,38 +10,44 @@ def printGreen(text, postfix=''): print("\033[92m {}\033[00m".format(text) + pos
 
 path = os.getcwd()
 
+# Make a list of the files and folders.
 files = [d for d in os.listdir(path) if os.path.isfile(os.path.join(path, d))]
 files.sort()
 folders = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 folders.sort()
 
+# Remove hidden files and folders from the lists.
 if not args.allfiles:
     files = [ f for f in files if "." != f[0]]
     folders = [ f for f in folders if "." != f[0]]
 
 readme_filename = 'README.md'
 
+# If there is a README.md file, display a short preview of it.
 if os.path.isfile(readme_filename):
     lines = open(readme_filename, "r").readlines()[:5]
     print("")
     printGreen(readme_filename)
     for line in lines:
-        if len(line.strip()) > 0: print(" " + line.rstrip())
+        if len(line.strip()) > 0: 
+            print(" " + line.rstrip())
 
 # Output to the console.
 print("")
 printGreen("Path: ", path)
 print("")
 
+# Find the maximum length names
 max_folder_name_length = 0
 for f in folders:
     if len(f) > max_folder_name_length:
-        max_folder_name_length = len(f) + 3
+        max_folder_name_length = len(f) + 4
 max_file_name_length = 0
 for f in files:
     if len(f) > max_file_name_length:
-        max_file_name_length = len(f) + 3
+        max_file_name_length = len(f) + 4
 
+# List folders.
 if len(folders) > 0:
     printGreen("Folders:")
     out = " "
@@ -49,8 +55,9 @@ if len(folders) > 0:
         out += f.ljust(max_folder_name_length)
     print(out)
 else:
-    print(" No folders.")
+    printGreen(" No folders.")
 
+# List Files
 if len(files) > 0:
     print("")
     printGreen("Files:")
@@ -60,5 +67,5 @@ if len(files) > 0:
     print(out)
 else:
     print("")
-    print(" No files.")
+    printGreen(" No files.")
 print("")
